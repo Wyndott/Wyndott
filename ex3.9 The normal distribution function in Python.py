580bceb776c78877e45b5ac3673003a0c96d7845
Wyndott <132722128+Wyndott@users.noreply.github.com>
2024-12-01 19:33:24 -0500

    Add files via upload

---
+++ ex3.9 The normal distribution function in Python.py
@@ -0,0 +1,3 @@
+# normal distribution, returns likeihood
+def normal_pdf(x: float, mean: float, std_dev: float) -> float:
+    return (1.0 / (2.0 * math.pi * std_dev ** 2) ** .05) * math.exp(-1.0 * ((x - mean) ** 2 / (2.0 * std_dev **2)))
